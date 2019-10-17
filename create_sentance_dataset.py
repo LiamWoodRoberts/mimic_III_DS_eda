6baed6228249693f294dfb5d8f57618e1bfdcced
Liam Roberts <Liamwr17@gmail.com>
2019-10-17 12:09:01 -0400

    added new pipeline for saving data for use in dataturks annotation service. Added model_training.py for developing spacy ner model. Moved data files to data folder

--- create_sentance_dataset.py
+++ create_sentance_dataset.py
@@ -1,6 +1,7 @@
 from section_parse import run
 import re
 import json
+import pandas as pd
 
 def clean_text(text):
     bad_chars = [":","*"]
@@ -15,11 +16,11 @@ def sections_to_sentances(sections):
     sentances = []
     for section in sections:
         section = clean_text(section)
-        sentances += [i.lstrip() for i in re.split("\.",section) if len(i)>0]
+        sentances += [i.lstrip() for i in re.split("\. ",section) if len(i)>0]
     return sentances
 
-def get_sentances(sections,n,min_len):
-    sections = medication_sections[:n]
+def get_sentances(sections,start,samples,min_len=3):
+    sections = medication_sections[start:start+n]
     seqs = sections_to_sentances(sections)
     seqs = [i for i in seqs if len(i)>min_len]
     return seqs
@@ -29,6 +30,13 @@ def write_sentances_to_txt(filename,seqs):
         f.writelines(f"{line}\n" for line in seqs)
     return print("~~~File Saved~~~")
 
+def create_and_save_df(sections,start,n,foldername="nlp_data"):
+    seqs = get_sentances(sections,start,n)
+    df = pd.DataFrame()
+    df["TEXT"] = seqs
+    df.to_csv(f"{foldername}/sentances_{start}-{start+n}.csv")
+    print("~~~File Saved Successfully")
+    return df
 
 if __name__ == "__main__":
     # Get Sections for Section Parse Module
@@ -37,7 +45,10 @@ if __name__ == "__main__":
     medication_sections = [i for i in medication_sections if i != "NOT FOUND"]
 
     # Convert Sections --> Sentances for NER
-    seqs = get_sentances(medication_sections,650,3)
+    start = 10
+    n = 10
+    seqs = get_sentances(medication_sections,start,n)
     print("Sentance Examples:\n")
-    [print(f"{i} - ",seqs[i],"\n") for i in range(3)]
-    write_sentances_to_txt("./unannotatted.txt",seqs)
+    [print(f"{i} - ",seqs[i],"\n") for i in range(5)]
+    
+    df = create_and_save_df(medication_sections,start,n,foldername="data")
---
+++ model_training.py
@@ -0,0 +1,82 @@
+import spacy
+import random
+import logging
+import json
+import time
+
+# Taken From: https://dataturks.com/help/dataturks-ner-json-to-spacy-train.php
+
+def convert_dataturks_to_spacy(dataturks_JSON_FilePath):
+    ''' 
+        Source: https://dataturks.com/help/dataturks-ner-json-to-spacy-train.php
+        Load Dataturks Annotated Data File and Returns "Spacy Formatted" Text
+    '''
+
+    try:
+        training_data = []
+        lines=[]
+        with open(dataturks_JSON_FilePath, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            data = json.loads(line)
+            text = data['content']
+            entities = []
+            for annotation in data['annotation']:
+                #only a single point in text annotation.
+                point = annotation['points'][0]
+                labels = annotation['label']
+                # handle both list of labels or a single label.
+                if not isinstance(labels, list):
+                    labels = [labels]
+
+                for label in labels:
+                    #dataturks indices are both inclusive [start, end] but spacy is not [start, end)
+                    entities.append((point['start'], point['end'] + 1 ,label))
+
+            training_data.append((text, {"entities" : entities}))
+
+        return training_data
+    except Exception as e:
+        logging.exception("Unable to process " + dataturks_JSON_FilePath + "\n" + "error = " + str(e))
+        return None
+
+def train_spacy(TRAIN_DATA,epochs=10):
+    ''' 
+        Source: https://dataturks.com/help/dataturks-ner-json-to-spacy-train.php
+        Accepts "Spacy Formatted" Training data and returns a trained Spacy NER model
+    '''
+    nlp = spacy.blank('en')  # create blank Language class
+    
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner, last=True)
+
+    # add labels
+    for _, annotations in TRAIN_DATA:
+        for ent in annotations.get('entities'):
+            ner.add_label(ent[2])
+    
+    optimizer = nlp.begin_training()
+    for itn in range(epochs):
+        print(f"Starting iteration: {itn}..." )
+        random.shuffle(TRAIN_DATA)
+        losses = {}
+        for text, annotations in TRAIN_DATA:
+            nlp.update(
+                [text],  # batch of texts
+                [annotations],  # batch of annotations
+                drop=0.2,  # dropout - make it harder to memorise data
+                sgd=optimizer,  # callable to update weights
+                losses=losses)
+        print(losses)
+    with open('data/model_losses.csv','a') as fd:
+        fd.write(f"{losses['ner']},{time.ctime()}")
+    return nlp
+
+if __name__ == "__main__":
+    data = convert_dataturks_to_spacy("./data/annotated_data/MimicNER_0-10.json")
+    nlp = train_spacy(data)
+    text = "Review of systems is negative for the following  Fevers, chills, nausea, vomiting, night sweats, change in weight, gastrointestinal complaints, neurologic changes, rashes, palpitations, orthopnea"
+    doc = nlp(text)
+    print ("Entities= " + str(["" + str(ent.text) + "->" + str(ent.label_) for ent in doc.ents]))
